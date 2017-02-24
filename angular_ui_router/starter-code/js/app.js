518e6ca14ccea44fc8f697c8e3c74719a5950bea
Qian Chen <oda_san@hotmail.com>
2017-02-24 11:34:54 -0800

    todos

--- angular_ui_router/starter-code/js/app.js
+++ angular_ui_router/starter-code/js/app.js
@@ -1,5 +1,22 @@
 (function() {
 	'use strict';
 
-	angular.module('todoApp', []);
+	angular.module('todoApp', ['ui.router'])
+  .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    // Routes
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+    })
+    .state('archive', {
+      url: '/archive',
+      templateUrl: 'archive.html'
+    });;
+
+    $urlRouterProvider.otherwise('/');
+  }
+
 }());
